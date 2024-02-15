ef5352c79603fe3525631078cf1994de77c9dc69
Michele Brigadoi <michele.brigadoi@unibo.it>
2024-02-15 16:11:14 +0100

    Integration container and flight delay project.

--- run.sh
+++ run.sh
@@ -4,15 +4,28 @@ cd ~/Workspace/DigitalTwin
 # Run containers
 docker-compose up -d
 sleep 5
-# Create topics
-docker exec -it broker kafka-topics --create --bootstrap-server localhost:9092 --topic flights
-docker exec -it broker kafka-topics --create --bootstrap-server localhost:9092 --topic weather
-docker exec -it broker kafka-topics --create --bootstrap-server localhost:9092 --topic flight_weather
 
-# Run producers and consumer
-gnome-terminal --tab -- bash -c "source venv/bin/activate; python modules/producers/flights_producer.py; exec bash"
-gnome-terminal --tab -- bash -c "source venv/bin/activate; python modules/producers/weather_producer.py; exec bash"
-gnome-terminal --tab -- bash -c "source venv/bin/activate; python modules/consumers/consumer.py; exec bash"
+# Flight Weather
+# # Create topics
+# docker exec -it broker kafka-topics --create --bootstrap-server localhost:9092 --topic flights
+# docker exec -it broker kafka-topics --create --bootstrap-server localhost:9092 --topic weather
+# docker exec -it broker kafka-topics --create --bootstrap-server localhost:9092 --topic flight_weather
+
+# # Run producers and consumer
+# gnome-terminal --tab -- bash -c "source venv/bin/activate; python pocs/flight_weather/producers/flights_producer.py; exec bash"
+# gnome-terminal --tab -- bash -c "source venv/bin/activate; python pocs/flight_weather/producers/weather_producer.py; exec bash"
+# gnome-terminal --tab -- bash -c "source venv/bin/activate; python pocs/flight_weather/consumers/consumer.py; exec bash"
+
+# # Run kafka-console-consumer
+# docker exec -it broker kafka-console-consumer --bootstrap-server localhost:9092 --topic flight_weather --from-beginning
+
+# Flight Delay
+docker exec -it broker kafka-topics --create --bootstrap-server localhost:9092 --topic flights
+docker exec -it broker kafka-topics --create --bootstrap-server localhost:9092 --topic delays
+
+# Run producer and consumer
+gnome-terminal --tab -- bash -c "source venv/bin/activate; python pocs/flight_delay/producer.py; exec bash"
+gnome-terminal --tab -- bash -c "source venv/bin/activate; python pocs/flight_delay/consumer.py; exec bash"
 
 # Run kafka-console-consumer
 docker exec -it broker kafka-console-consumer --bootstrap-server localhost:9092 --topic flight_weather --from-beginning
--- stop.sh
+++ stop.sh
@@ -2,9 +2,14 @@
 cd ~/Workspace/DigitalTwin
 
 # Delete topics
+# # Flight Weather
+# docker exec -it broker kafka-topics --delete --bootstrap-server localhost:9092 --topic flights
+# docker exec -it broker kafka-topics --delete --bootstrap-server localhost:9092 --topic weather
+# docker exec -it broker kafka-topics --delete --bootstrap-server localhost:9092 --topic flight_weather
+
+# Flight Delay
 docker exec -it broker kafka-topics --delete --bootstrap-server localhost:9092 --topic flights
-docker exec -it broker kafka-topics --delete --bootstrap-server localhost:9092 --topic weather
-docker exec -it broker kafka-topics --delete --bootstrap-server localhost:9092 --topic flight_weather
+docker exec -it broker kafka-topics --delete --bootstrap-server localhost:9092 --topic delays
 
 # Teardown Kafka services
 docker-compose down -v
