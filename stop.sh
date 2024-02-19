f76772e5bbe1b34e220c15a8a7901e125bb7d379
Michele Brigadoi <michele.brigadoi@unibo.it>
2024-02-19 11:15:18 +0100

    Config run and stop cmds.

--- stop.sh
+++ stop.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Check if the number of arguments is correct
+# Check if Docker or Podman is used
 if [ "$1" == "docker" ]; then
     echo "Using Docker"
     CMD="docker "
@@ -12,22 +12,13 @@ fi
 # Select working directory
 cd ~/Workspace/DigitalTwin
 
+# Delete topics
 TOPICS=$($CMD exec broker bash -c "kafka-topics --list --bootstrap-server localhost:9092" )
 
 for T in $TOPICS
 do
     $CMD exec -it broker kafka-topics --delete --bootstrap-server localhost:9092 --topic $T
 done
-
-# Delete topics
-# # Flight Weather
-# podman exec -it broker kafka-topics --delete --bootstrap-server localhost:9092 --topic flights
-# podman exec -it broker kafka-topics --delete --bootstrap-server localhost:9092 --topic weather
-# podman exec -it broker kafka-topics --delete --bootstrap-server localhost:9092 --topic flight_weather
-
-# Flight Delay
-# podman exec -it broker kafka-topics --delete --bootstrap-server localhost:9092 --topic flights
-# podman exec -it broker kafka-topics --delete --bootstrap-server localhost:9092 --topic delays
 
 # Teardown Kafka services
 $CMD compose down -v --remove-orphans
